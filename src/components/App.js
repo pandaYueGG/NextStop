f3eaad5547a4133ee58da6a13ddd4c106f86f8f5
nickwai1 <53497786+nickwai1@users.noreply.github.com>
2021-10-16 12:06:48 -0700

    added folder structure for the frontend

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Header from './Header/header.js';
+import Homepage from './Homepage/homepage.js';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
