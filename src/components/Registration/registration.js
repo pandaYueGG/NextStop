eb678fbc8e41726f25007b1459d997cd1ccc55ac
nickwai1 <53497786+nickwai1@users.noreply.github.com>
2021-10-19 12:42:54 -0700

    basic tailwind styling on signup page

--- src/components/Registration/registration.js
+++ src/components/Registration/registration.js
@@ -10,38 +10,29 @@ function registration() {
     const [newPassword, setNewPassword] = useState('');
 
     return (
-        <div >
-            <div className="grid grid-rows-3 grid-flow">
-                <h4>Create an Account</h4>
-                <label>Username</label>
-                <input className="form-control" type="text"
-                onChange={(e) => {
-                    setNewUsername(e.target.value);
-                }}
-                />
-                <label>Email</label>
-                <input className="form-control" type="text"
-                onChange={(e) => {
-                    setNewEmail(e.target.value);
-                }}
-                />
-                <label>Phone Number</label>
-                <input className="form-control" type="text"
-                onChange={(e) => {
-                    setNewPhoneNumber(e.target.value);
-                }}
-                />
-                <label>Password</label>
-                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text"
-                onChange={(e) => {
-                    setNewPassword(e.target.value);
-                }}
-                />
-                <div className="mt-4 text-center">
-                    <button type="button" className="bg-black rounded-md p-2 inline-flex items-center justify-center text-white">Register</button>
+        <div className="mt-8 mx-5">
+            <div className="grid grid-cols-1 bg-white rounded overflow-hidden shadow-md mx-52 py-5">
+                <div className="grid grid-rows-3 grid-flow px-5">
+                    <div className="text-lg font-bold">Create an Account</div>
+                    <label>Username</label>
+                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text"
+                    onChange={(e) => {
+                        setNewUsername(e.target.value);
+                    }}
+                    />
+                    <label>Password</label>
+                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text"
+                    onChange={(e) => {
+                        setNewPassword(e.target.value);
+                    }}
+                    />
+                    <div className="mt-4 text-center">
+                        <button type="button" className="bg-black rounded-md p-2 inline-flex items-center justify-center text-white">Register</button>
+                    </div>
                 </div>
             </div>
         </div>
+
     )
 }
 
