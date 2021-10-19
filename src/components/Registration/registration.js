ffba33a2ae522b710d41fb8913fc286d6532c8a4
nickwai1 <53497786+nickwai1@users.noreply.github.com>
2021-10-19 12:50:00 -0700

    add more styles to signup page

--- src/components/Registration/registration.js
+++ src/components/Registration/registration.js
@@ -11,23 +11,25 @@ function registration() {
 
     return (
         <div className="mt-8 mx-5">
-            <div className="grid grid-cols-1 bg-white rounded overflow-hidden shadow-md mx-52 py-5">
-                <div className="grid grid-rows-3 grid-flow px-5">
+            <div className="grid grid-cols-1 bg-white rounded overflow-hidden shadow-md mx-52 py-20">
+                <div className="grid grid-rows-3 grid-flow px-24">
                     <div className="text-lg font-bold">Create an Account</div>
-                    <label>Username</label>
+                    <label className="flex justify-start">Username</label>
                     <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text"
+                    placeholder="username"
                     onChange={(e) => {
                         setNewUsername(e.target.value);
                     }}
                     />
-                    <label>Password</label>
+                    <label className="flex justify-start mt-5">Password</label>
                     <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text"
+                    placeholder="password"
                     onChange={(e) => {
                         setNewPassword(e.target.value);
                     }}
                     />
-                    <div className="mt-4 text-center">
-                        <button type="button" className="bg-black rounded-md p-2 inline-flex items-center justify-center text-white">Register</button>
+                    <div className="mt-5 text-center">
+                        <button type="button" className="bg-black rounded-md py-2 px-32 inline-flex items-center justify-center text-white">Register</button>
                     </div>
                 </div>
             </div>
