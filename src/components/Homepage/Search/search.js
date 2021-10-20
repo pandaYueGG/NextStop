67e3e10b74b2ffe130f50182427a883328ed76f3
Crystal Lee <crystallee@Crystals-MBP.hsd1.ca.comcast.net>
2021-10-20 11:54:35 -0700

    Added icon to leaving from/going to inputs, added more responsiveness for small screens

--- src/components/Homepage/Search/search.js
+++ src/components/Homepage/Search/search.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './Styled/search.css';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
+
 const Search = () => {
   const [arrivalDate, setArrivalDate] = React.useState('');
   const [departureDate, setDepartureDate] = React.useState('');
@@ -8,18 +11,26 @@ const Search = () => {
 
   return (
     <div className="banner justify-center xl:justify-start">
-      <div className="search w-4/5 h-4/5 xl:w-1/4 xl:h-3/4">
+      <div className="search w-full h-auto sm:w-4/5 sm:h-4/5 xl:w-2/6 xl:h-3/4">
         <div className="search-body">
           <div className="my-2">
             <h1>Let's plan out your flights and hotels so you don't miss your next event.</h1>
           </div>
-          <div className="my-2">
-            <h3>Destination</h3>
-              <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="Depart" />
+          <div class="relative flex w-full flex-wrap items-stretch mb-3">
+            <span class="z-10 h-full leading-snug font-normal absolute text-center text-blueGray-300 absolute bg-transparent rounded text-base items-center justify-center w-8 pl-3 py-3">
+              <FontAwesomeIcon icon={faMapMarkerAlt} />
+            </span>
+            <input type="text" placeholder="Leaving from" class="px-3 py-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-sm border border-blueGray-300 outline-none focus:outline-none focus:ring w-full pl-10"/>
+          </div>
+          <div class="relative flex w-full flex-wrap items-stretch mb-3">
+            <span class="z-10 h-full leading-snug font-normal absolute text-center text-blueGray-300 absolute bg-transparent rounded text-base items-center justify-center w-8 pl-3 py-3">
+              <FontAwesomeIcon icon={faMapMarkerAlt} />
+            </span>
+            <input type="text" placeholder="Going to" class="px-3 py-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-sm border border-blueGray-300 outline-none focus:outline-none focus:ring w-full pl-10"/>
           </div>
           <div className="my-2 grid grid-cols-2 grid-template-rows-2 gap-x-2">
-            <h3>Arrive</h3>
-            <h3>Depart</h3>
+            <h3>Departing</h3>
+            <h3>Returning</h3>
             <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="" />
             <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="" />
           </div>
