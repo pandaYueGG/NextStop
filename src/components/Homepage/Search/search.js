efaa451a72b5172f1a5de9d771b93957bb366a97
Crystal Lee <crystallee@Crystals-MBP.hsd1.ca.comcast.net>
2021-10-20 10:01:37 -0700

    Changed threshold for responsive resizing, fixed button sizes

--- src/components/Homepage/Search/search.js
+++ src/components/Homepage/Search/search.js
@@ -7,8 +7,8 @@ const Search = () => {
   const [destination, setDestination] = React.useState('');
 
   return (
-    <div className="banner justify-center sm:justify-start">
-      <div className="search w-4/5 h-4/5 sm:w-1/4 sm:h-3/4">
+    <div className="banner justify-center xl:justify-start">
+      <div className="search w-4/5 h-4/5 xl:w-1/4 xl:h-3/4">
         <div className="search-body">
           <div className="my-2">
             <h1>Let's plan out your flights and hotels so you don't miss your next event.</h1>
@@ -24,8 +24,8 @@ const Search = () => {
             <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="" />
           </div>
           <div className="flex flex-row my-4">
-              <button className="py-2 px-3 text-white bg-red-400 hover:bg-red-500 rounded mr-5">Find Flights</button>
-              <button className="py-2 px-3 text-white bg-red-400 hover:bg-red-500 rounded mr-5">Find Hotels</button>
+              <button className="py-3 px-3 text-white bg-red-400 hover:bg-red-500 rounded mr-5">Find Flights</button>
+              <button className="py-3 px-3 text-white bg-red-400 hover:bg-red-500 rounded mr-5">Find Hotels</button>
           </div>
         </div>
       </div>
