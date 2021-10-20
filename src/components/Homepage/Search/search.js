e02ecf6359817372005f91ff2dad0bd5715e7c0b
Crystal Lee <crystallee@Crystals-MBP.hsd1.ca.comcast.net>
2021-10-20 12:53:28 -0700

    Added date pickers to departure and return

--- src/components/Homepage/Search/search.js
+++ src/components/Homepage/Search/search.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { enGB } from 'date-fns/locale';
+import { DatePicker } from 'react-nice-dates';
+import 'react-nice-dates/build/style.css';
 import './Styled/search.css';
+import { format } from 'date-fns';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+import Select from 'react-select';
 
 const Search = () => {
-  const [arrivalDate, setArrivalDate] = React.useState('');
-  const [departureDate, setDepartureDate] = React.useState('');
-  const [destination, setDestination] = React.useState('');
+  const [arrivalDate, setArrivalDate] = useState('');
+  const [departureDate, setDepartureDate] = useState(new Date());
+  const [destination, setDestination] = useState(new Date());
+
+  const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' }
+  ]
 
   return (
     <div className="banner justify-center xl:justify-start">
@@ -31,12 +42,31 @@ const Search = () => {
           <div className="my-2 grid grid-cols-2 grid-template-rows-2 gap-x-2">
             <h3>Departing</h3>
             <h3>Returning</h3>
-            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="" />
-            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="" />
+            <DatePicker date={departureDate} onDateChange={setDepartureDate} locale={enGB}>
+              {({ inputProps, focused }) => (
+                <input 
+                  className={'input' + (focused ? ' -focused' : '') + 'shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'}
+                  {...inputProps}
+                />
+              )}
+            </DatePicker>
+            <DatePicker date={arrivalDate} onDateChange={setArrivalDate} locale={enGB}>
+              {({ inputProps, focused }) => (
+                <input 
+                  className={'input' + (focused ? ' -focused' : '') + 'shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'}
+                  {...inputProps}
+                />
+              )}
+            </DatePicker>
+            {/* <DatePicker onChange={setDepartureDate} value={departureDate} />
+            <DatePicker onChange={setArrivalDate} value={arrivalDate} /> */}
+            {/* <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="" />
+            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="" /> */}
           </div>
           <div className="flex flex-row my-4">
               <button className="py-3 px-3 text-white bg-red-400 hover:bg-red-500 rounded mr-5">Find Flights</button>
               <button className="py-3 px-3 text-white bg-red-400 hover:bg-red-500 rounded mr-5">Find Hotels</button>
+              <button className="py-3 px-3 text-white bg-red-400 hover:bg-red-500 rounded mr-5">Find Events</button>
           </div>
         </div>
       </div>
