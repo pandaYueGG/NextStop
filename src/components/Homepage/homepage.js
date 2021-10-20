b7089a8cc1fdcd3375a20ed5aedcdc8e7aa6b065
Robin Jeng <robinjeng0@gmail.com>
2021-10-20 10:06:39 -0700

    Add import search bar on homepage

--- src/components/Homepage/homepage.js
+++ src/components/Homepage/homepage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Flights from '../Homepage/Flights/Flights.js';
 import Hotels from '../Homepage/Hotels/Hotels.js';
 import Events from '../Homepage/Events/Events.js';
-import Search from '../Homepage/Search/Search.js';
+import Search from '../Homepage/Search/search.js';
 
 function homepage() {
   return (
