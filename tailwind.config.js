5f5e36fb2566b9679c4b3dba5d5d9f3b7502c46a
nickwai1 <53497786+nickwai1@users.noreply.github.com>
2021-10-21 10:10:30 -0700

    styles for profile page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateRows: {
+        // Simple 8 row grid
+       '3': 'repeat(3, minmax(0, 1fr))',
+
+        // Complex site-specific row configuration
+       'layout': '100px minmax(200px, 1fr) 100px',
+      }
+    },
   },
   variants: {
     extend: {},
