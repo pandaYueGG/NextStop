5b4d001b5e9b97d5daeb0719ef7af8c736bca435
nickwai1 <53497786+nickwai1@users.noreply.github.com>
2021-10-21 14:46:32 -0700

    basic styling template for confirmation page

--- src/components/BackHome.js
+++ src/components/BackHome.js
@@ -24,9 +24,29 @@ const BackHome = () => {
   let history = useHistory();
 
   return (
-    <div className="text-3xl bold m-auto">
-      <span>Thank You For Your Payment !</span><br />
-      <span>Enjoy Your Trip</span>
+    <div className="mt-10">
+        <div className="border rounded-lg bg-white shadow-lg overflow-hidden mx-52">
+            <div className="mt-10 text-4xl font-bold mx-auto">Your Booking Confirmation</div>
+                <div className="mx-10 border rounded-lg bg-white shadow-lg overflow-hidden py-16 my-10">
+                    <div className="h-64 grid grid-rows-4 grid-flow-col gap-4 my-20">
+                      <div className="text-2xl font-bold">
+                        London, here you come!
+                      </div>
+                      <div className="font-bold">
+                      Thank you for booking with NextStop. Please check your email for a confirmation from your airline.
+                      </div>
+                      <div className="mx-40">
+                          Essential information to enter the UK
+                          The United Kingdom is taking steps to help stop the spread of COVID-19.
+
+                          To protect your health and others', everyone must complete an online passenger locator form before arrival in the United Kingdom.
+                          You may be required to self-isolate for 14 days on arrival. Check the exempt countries list immediately before you travel as this list can change at short notice.
+                          It is a legal requirement that you wear a face covering on public transport in the UK.
+                          Failure to comply with the above measures is a criminal offence and you could be fined. Please visit www.gov.uk/uk-border-control for detailed public health advice and requirements for entering the UK.
+                      </div>
+                    </div>
+                </div>
+        </div>
       <Button  onClick={() => {history.push("/")}} className="pay-btn">Back to Homepage</Button>
     </div>
   );
